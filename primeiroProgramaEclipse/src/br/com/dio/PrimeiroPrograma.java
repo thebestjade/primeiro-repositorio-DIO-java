a796e7a825d916d2c5b1e096e311f0a46adff645
Jade Cordeiro <jade.dev@hotmail.com>
2021-11-24 20:09:36 -0300

    primeiroProgramaEclipse

---
+++ primeiroProgramaEclipse/src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,21 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+	public static void main(String[] args) {
+		Gato gato = new Gato();
+		Livros livros = new Livros();
+		System.out.println(gato);
+		System.out.println(livros);
+		
+		/*int a = 3;
+		int b = 3;
+		System.out.println("Hello worlds! total: " + (a+b));*/
+	}
+}
+
+class Livros{
+	private String nome;
+	private Integer nPag;
+}
