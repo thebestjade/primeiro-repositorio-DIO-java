84babfd6ef4ccbf2a9c18b725a78540e682d2a23
Jade Cordeiro <jade.dev@hotmail.com>
2021-11-25 18:07:13 -0300

    primeiro programa java no intellij

--- PrimeiroProgramaIntellij/src/br/com/dio/PrimeiroPrograma.java
+++ PrimeiroProgramaIntellij/src/br/com/dio/PrimeiroPrograma.java
@@ -8,8 +8,45 @@ public class PrimeiroPrograma {
 
         System.out.println(gato);
 
+        Livro livro1= new Livro("Harry Potter e a pedra filosofal", 350);
+
+        System.out.println(livro1);
        /* int a = 5;
         int b = 3;
         System.out.println("Hello world! Soma= " + (a+b));*/
+    }
+}
+
+class Livro {
+    private String nome;
+    private Integer numPag;
+
+    public Livro(String nome, Integer numPag) {
+        this.nome = nome;
+        this.numPag = numPag;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumPag() {
+        return numPag;
+    }
+
+    public void setNumPag(Integer numPag) {
+        this.numPag = numPag;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numPag=" + numPag +
+                '}';
     }
 }
